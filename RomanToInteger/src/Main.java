5524e7d7a27f6fdab92ffb2c272ffa054589400c
Mehmet Emin KETRİZ <89727826+mehmeteminketriz@users.noreply.github.com>
2023-12-05 12:57:51 +0300

    Update Main.java

--- RomanToInteger/src/Main.java
+++ RomanToInteger/src/Main.java
@@ -76,15 +76,3 @@ public class Main {
         return ans;
     }
 }
-/*
-Bu değişkenler, Roma rakamlarını sayıya çevirme işlemi sırasında geçici olarak kullanılan değerlerdir. İşlevleri şu şekildedir:
-
-num: Güncellenen karakterin Roma rakamı karşılığını tutar. Örneğin, 'I' için 1, 'V' için 5, 'X' için 10, vb.
-prevNum: Bir önceki karakterin Roma rakamı karşılığını tutar. Bu, güncellenen karakterin değeri ile bir öncekini karşılaştırmak için kullanılır.
-ans: Toplam sayıyı temsil eder. Her bir karakterin değeri, toplam sayıya eklenir veya çıkarılır.
-Örneğin, 'IV' (4) sayısını düşünelim:
-
-İlk olarak 'V' (5) işlemine gelindiğinde, num değeri 5, prevNum değeri 1, ans değeri 5 olur. Çünkü 5'ten önce 1 geldiği için 5 eklenir.
-Daha sonra 'I' (1) işlemine gelindiğinde, num değeri 1, prevNum değeri 5, ans değeri 4 olur. Çünkü 1'den önce 5 geldiği için 1 çıkarılır.
-Bu değişkenler, Roma rakamlarını sayıya çevirirken karşılaşılan her karakter için geçici olarak saklanan değerlerdir.
- */
